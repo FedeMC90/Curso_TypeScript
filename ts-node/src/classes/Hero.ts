6714c37c83a5f1875c54906f271f2f6f4446933d
Federico Ciociano <fmciociano@falabella.com.ar>
2023-09-22 14:57:00 -0300

    commit

--- ts-node/src/classes/Hero.ts
+++ ts-node/src/classes/Hero.ts
@@ -1,8 +1,20 @@
+import powers from "../data/powers";
+
 export class Hero {
-  constructor (
+  constructor(
     public name: string,
     public powerId: number,
     public age: number
   ) {}
+
+  get power(): string {
+    return powers.find(power => power.id === this.powerId )?.desc || 'Not Found';
+  }
 }
 
+export class Hero2 {}
+export class Hero3 {}
+export class Hero4 {}
+
+const PI = 3.1415;
+const miNombre = "Federico";
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,4 +1,15 @@
-import { Hero } from "./classes/Hero";
+// Si llego a tener duplicado el valor de lo que estoy importando o simplemente quiero darle otro nombre puedo importarlo con un alias usando la palabra reservada "as"
+//? import { Hero as Superhero } from "./classes/Hero";
 
-const ironman = new Hero("Ironman", 1, 55);
-console.log('ironman: ', ironman);
+// Otra forma de importar es trayendome todo y le asigno un alias. De esta manera lo que importe se comportará como si fuesen métodos de una clase
+import * as HeroClasses from "./classes/Hero";
+
+// También está el "export default". De esta manera al importar, no hace falta usar destructuring y puedo darle a la importación el nombre que yo quiera que JS va a entender lo que estoy importando porque viene por default (valga la redundancia)
+import poderes from "./data/powers";
+
+const Hero = 123;
+
+const ironman = new HeroClasses.Hero("Ironman", 1, 55);
+console.log("ironman: ", ironman);
+
+console.log(poderes)
