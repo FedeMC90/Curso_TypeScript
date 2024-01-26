1458689816315d99c60cb25a2789b47765e3ae0e
Federico <fedemc90@gmail.com>
2024-01-26 10:36:04 -0300

    Update function-type.ts

--- Bases/Funciones/function-type.ts
+++ Bases/Funciones/function-type.ts
@@ -7,9 +7,10 @@
     // Define el tipo de variable de tipo función
     // let myFunction: Function;
 
-    // De esta manera defino la variable como tipo "función" y el tipo de dato que deberá devolver
+    // Defino la variable como tipo "función" y el tipo de dato que deberá devolver
     let myFunction: ( x: number, y: number ) => number;
 
+    // Como myFunction es una variable de tipo función puedo asignarle cualquier valor que sea de tipo función
     // myFunction = 10;
     // console.log('myFunction: ', myFunction);
 
